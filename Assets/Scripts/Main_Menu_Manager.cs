014f5d8d41fede10bd191d781b3b7754bfb9a369
SaiLikesLego <Isak.Sivapragasam@elev.ga.lbs.se>
2025-11-03 12:13:24 +0100

    prefabs

--- Assets/Scripts/Main_Menu_Manager.cs
+++ Assets/Scripts/Main_Menu_Manager.cs
@@ -4,13 +4,13 @@ using UnityEngine.SceneManagement;
 
 public class Main_Menu_Manager : MonoBehaviour
 {
-
+    // Starts the game
     public void Starting()
     {
         SceneManager.LoadSceneAsync("Isak");
     }
 
-    //FUCKING QUITS THE GAME DUH.
+    // FUCKING QUITS THE GAME DUH.
     public void Quiter()
     {
         Application.Quit();
