d8c8a3e11fd49f3194398c0ca6b3a1e73c95a717
CoffeeToast2288 <156317180+CoffeeToast2288@users.noreply.github.com>
2025-11-04 11:48:49 +0100

    Player health script + enemies punch you
    Hihihaw

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float Hp;
+    public float Hp_max;
+    PlayerStats stats;
+    bool dead = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Hp > Hp_max)
+        {
+
+            Hp = Hp_max;
+
+        }
+        Hp_max += stats.hp;
+        if(Hp<= 0)
+        {
+            dead = true;
+        }
+    }
+
+
+    public void Damage(float damage)
+    {
+        Hp -= damage;
+
+    }
+}
