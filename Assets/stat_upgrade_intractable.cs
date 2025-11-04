1fdb21f442ec7bd50cc4c5bfcc77915a545ca2d1
ViCeshogun <156317435+ViCeshogun@users.noreply.github.com>
2025-11-04 12:49:03 +0100

    intractables

--- Assets/stat_upgrade_intractable.cs
+++ Assets/stat_upgrade_intractable.cs
@@ -2,12 +2,12 @@ using UnityEngine;
 
 public class stat_upgrade_intractable : MonoBehaviour
 {
-
+    public GameObject upgrade_station_ui;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        upgrade_station_ui.SetActive(false);
     }
 
     // Update is called once per frame
@@ -18,7 +18,11 @@ public class stat_upgrade_intractable : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.tag == "Player")
+        {
+            upgrade_station_ui.SetActive(true);
+
+        }
 
 
 
