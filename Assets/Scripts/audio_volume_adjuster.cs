3161bc9b85412c4a57723f88a1dda95694198ce5
ViCeshogun <156317435+ViCeshogun@users.noreply.github.com>
2025-11-04 10:30:11 +0100

    background music done

--- Assets/Scripts/audio_volume_adjuster.cs
+++ Assets/Scripts/audio_volume_adjuster.cs
@@ -5,10 +5,10 @@ using UnityEngine.Rendering;
 public class audio_volume_adjuster : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public audiocontroler audio_stats;
-    public float volume;
-    public AudioSource self;
-    public string sound_type;
+    public audiocontroler audio_stats; // this refrances the audiocontroler script wich controls volume 
+    public float volume; // this is the volume for the audio 
+    public AudioSource self; // this is the audiosorce on the same object as this script 
+    public string sound_type; // this determis what volume type it uses 
 
     void Start()
     { 
@@ -19,7 +19,7 @@ public class audio_volume_adjuster : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       if( sound_type == "monster")
+       if( sound_type == "monster") // finds the matching type of volume it needs and adjusts it 
         {
             volume = audio_stats.master_volume * audio_stats.monster_volume;
            
@@ -39,7 +39,7 @@ public class audio_volume_adjuster : MonoBehaviour
             volume = audio_stats.master_volume * audio_stats.effect_volume;
 
         }
-        self.volume = volume;
+        self.volume = volume; // sets the volume of the audiosorce
 
     }
 }
--- Assets/Scripts/audiocontroler.cs
+++ Assets/Scripts/audiocontroler.cs
@@ -5,9 +5,9 @@ using UnityEngine.UI;
 
 public class audiocontroler : MonoBehaviour
 {
-    public List<AudioSource> audio_list = new List<AudioSource>();
-    public float master_volume, monster_volume, player_volume, level_volume, effect_volume;
-    public Scrollbar master_scroll, monster_scroll, player_scroll, level_scroll, effect_scroll;
+    public List<AudioSource> audio_list = new List<AudioSource>(); // list of all the sounds
+    public float master_volume, monster_volume, player_volume, level_volume, effect_volume; // all the volume types including master volume 
+    public Scrollbar master_scroll, monster_scroll, player_scroll, level_scroll, effect_scroll; // the scrollbars 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,23 +19,23 @@ public class audiocontroler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-     if (master_scroll.value != master_volume)
+     if (master_scroll.value != master_volume) // these if and if else statments check if the scrollbar has been moved 
         {
             master_volume_change();
         }
-     if  (monster_volume != monster_scroll.value)
+     else if  (monster_volume != monster_scroll.value)
         {
             monster_volume_change();
         }
-     if (player_volume != player_scroll.value)
+     else if (player_volume != player_scroll.value)
         {
             player_volume_change();
         }
-     if (level_volume != level_scroll.value)
+    else if (level_volume != level_scroll.value)
         {
             level_volume_change();
         }
-     if (effect_volume != effect_scroll.value)
+    else if (effect_volume != effect_scroll.value)
         {
             effect_volume_change();
         }
@@ -43,6 +43,7 @@ public class audiocontroler : MonoBehaviour
 
     }
 
+    // the methods below change the value of volumes 
     public void master_volume_change()
     {
         master_volume = master_scroll.value;
