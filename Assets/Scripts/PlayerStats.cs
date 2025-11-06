a8c6fabf334448f15bb1ee2a2cdd73214126539e
CoffeeToast2288 <156317180+CoffeeToast2288@users.noreply.github.com>
2025-11-06 13:37:40 +0100

    stats stuff

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -6,16 +6,29 @@ using UnityEngine.Rendering;
 [System.Serializable]
 public class PlayerStats : MonoBehaviour
 {
- 
+    [Header("Refrences")]
+   
 
     
     //stats 
     public float damage = 1; 
-    public int hp = 0;
-    public int speed = 0; 
-    public int dash_chargers = 0;
-    public float dash_coldown_reduction = 0;
-    public float attack_speed = 0;
+    public int hp = 4;
+    public int speed = 5; 
+    public int dash_chargers = 1;
+    public float dash_coldown_reduction = 1;
+    public float attack_speed = 1;
 
-    
+    public bool upgraded = false;
+
+
+   private void Update()
+    {
+        if(upgraded == true)
+        {
+
+
+
+        }
+    }
+
 }
